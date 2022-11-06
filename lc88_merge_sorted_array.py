deb9145805d17615cf2fcfbfb77abe471c7acf3e
Pranav Shah <pranavshah01@gmail.com>
2022-11-06 04:10:38 -0800

    Bug fix for empty array

--- lc88_merge_sorted_array.py
+++ lc88_merge_sorted_array.py
@@ -7,28 +7,30 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         total, l1, l2 = m + n - 1, m - 1, n - 1
+
         while l1 >= 0 and l2 >= 0:
-            print(l1, nums1[l1], l2, nums2[l2])
             if nums1[l1] > nums2[l2]:
                 nums1[total] = nums1[l1]
-                total = total - 1
-                l1 = l1 - 1
-            elif nums1[l1] < nums2[l2]:
+                total, l1 = total - 1, l1 - 1
+
+            elif nums1[l1] == nums2[l2]:
                 nums1[total] = nums2[l2]
-                total = total - 1
-                l2 = l2 - 1
+                nums1[total - 1] = nums1[l1]
+                total, l1, l2 = total - 2, l1 - 1, l2 - 1
+
             else:
                 nums1[total] = nums2[l2]
-                nums1[total-1] = nums1[l1]
-                total = total - 2
-                l1 = l1 - 1
-                l2 = l2 - 1
+                total, l2 = total - 1, l2 - 1
+
+        while l2 >= 0:
+            nums1[total] = nums2[l2]
+            total, l2 = total - 1, l2 - 1
 
 
 if __name__ == "__main__":
-    nums1 = [1, 1, 9, 9, 0, 0, 0, 0]
-    m = 4
-    nums2 = [3, 4, 6, 9]
-    n = 4
+    nums1 = [0]
+    m = 0
+    nums2 = [1]
+    n = 1
     Solution().merge(nums1, m, nums2, n)
     print(nums1)
